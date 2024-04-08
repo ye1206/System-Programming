3d2b26c98bd8f985c1b15d34ef279225ada915c1
ALICE\alice <alicecheng.126@gmail.com>
2024-04-08 14:45:33 +0800

    upload system programming homework

---
+++ SP_HW01_110213076/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SP_HW01_110213076
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            string mnem;
+            Dictionary<string, string> opcodeTable = new Dictionary<string, string>(); //store the opcode table
+
+            StreamReader fileReader = File.OpenText("opCode.txt");
+            char[] delim = {',', '\t', ';', '\r', ' '}; //Record delimiter
+            while(!fileReader.EndOfStream)
+            {      
+                string line = fileReader.ReadLine();
+
+                if (!string.IsNullOrEmpty(line))
+                {
+                    string[] inputField = line.Split(delim);
+                    opcodeTable.Add(inputField[0], inputField[1]); //add the mnemonic code and opcode to the dictionary
+                } //if
+                else
+                    break;
+            }
+
+            fileReader.Close();
+            
+            Console.Write("Enter the mnemonic code: ");
+            mnem = Console.ReadLine();
+
+            if (opcodeTable.ContainsKey(mnem))
+                Console.WriteLine("The opCode is: " + opcodeTable[mnem]);
+            else
+                Console.WriteLine("Invalid mnemonic code!");
+        } //main
+    }
+}
+
+
+
+
---
+++ SP_HW02_110213076/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SP_HW02_110213076
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> symbolTable = new Dictionary<string, string>();
+            string input;
+            string search;
+            int i = 0;
+
+            Console.Write("Please input label and address: ");
+            input = Console.ReadLine();
+            string[] data = input.Split(' '); // Split the input by space
+            for (i = 0; i < data.Length; i += 2)
+                symbolTable.Add(data[i], data[i + 1]); // Add the label and address to the symbol table
+
+            Console.Write("Search (input the label): ");
+            search = Console.ReadLine();
+            if (symbolTable.ContainsKey(search)) // Check if the label exists in the symbol table
+                Console.WriteLine("Address: " + symbolTable[search]); // Print the address
+            else
+                Console.WriteLine("Invalid");
+
+            Console.ReadKey();
+        }
+    }
+}
